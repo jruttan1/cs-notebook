c695a60a5c1ff52f27cdad9382840a7b8356ef75
jruttan1 <jruttan3@uwo.ca>
2026-03-27 17:02:10 -0400

    feat: add leetcode #15: 3sum

---
+++ leetcode/3_sum.py
@@ -0,0 +1,32 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        #this is the worst question ever
+        nums.sort()
+        solutions = []
+        for i in range(len(nums)):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            left = i+1
+            right = len(nums) - 1
+            while left<right:
+                if nums[left] + nums[right] == -nums[i]:
+                    solutions.append([nums[i], nums[left], nums[right]])
+
+                    left+=1
+                    while left<right and nums[left] == nums[left-1]:
+                        left += 1
+
+                    right -= 1 
+                    while left<right and nums[right] == nums[right+1]:
+                        right -= 1
+
+                
+                elif nums[left] + nums[right] < -nums[i]:
+                    left += 1
+
+                elif nums[left] + nums[right] > -nums[i]:
+                    right -= 1
+
+        return solutions
+            
+#this was super brutal ngl
